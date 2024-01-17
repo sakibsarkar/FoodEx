b3534e9662e15c931fc1a87ee52eb5e8d6c7939c
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-17 12:11:59 +0600

    adding the request in vendor list

--- src/Cards/VendorRequestCard/VendorRequestCard.jsx
+++ src/Cards/VendorRequestCard/VendorRequestCard.jsx
@@ -1,5 +1,6 @@
 import "./VendorRequestCard.css";
 import UseAxios from "../../Hooks & Functions/useAxios";
+import { useState } from "react";
 import { FaHandshake } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
 import { RxCrossCircled } from "react-icons/rx";
@@ -9,13 +10,38 @@ import { toast } from "sonner";
 const VendorRequestCard = ({ data, refetch }) => {
     const { _id, owner_name, owner_email, vendor_name, logo, banner, status } = data
 
+    const [processing, setProcessing] = useState(false)
 
     const axios = UseAxios()
 
     const handleAction = async (action) => {
+        if (processing) {
+            return;
+        }
+
+        setProcessing(true)
         const toastId = toast.loading("Processing...")
         try {
+            // change the request status
             const { data: updateData } = await axios.put("/req/process", { id: _id, status: action })
+
+            // if accepted creat add new vendor
+
+            if (action === "accepted") {
+                const vendorData = {
+                    owner_name,
+                    owner_email,
+                    vendor_name,
+                    logo,
+                    banner,
+                    varyfied: true,
+                    likedBy: []
+                }
+                await axios.post("/new/vendor", vendorData)
+
+                await axios.put("/user/role", { email: owner_email, role: "vendor" })
+            }
+
             toast.dismiss(toastId)
             toast.success("Successfull")
             refetch()
@@ -25,6 +51,7 @@ const VendorRequestCard = ({ data, refetch }) => {
             console.log(err);
             toast.dismiss(toastId)
             toast.error("Something went wrong")
+            setProcessing(false)
         }
 
 

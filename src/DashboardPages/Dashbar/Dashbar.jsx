49b906489ce9a1d62c2e99fe2dc20e19d9f2fa00
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-17 12:30:21 +0600

    dashbar style added

--- src/DashboardPages/Dashbar/Dashbar.jsx
+++ src/DashboardPages/Dashbar/Dashbar.jsx
@@ -1,12 +1,25 @@
 import "./Dashbar.css";
+import { FaRegUser } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa6";
+import { FaShopLock } from "react-icons/fa6";
+import { GoGitPullRequest } from "react-icons/go";
 import { NavLink } from "react-router-dom";
 
 const Dashbar = () => {
     return (
         <div className="dashbarContainer">
-            <div className="dashLinks">
-                <NavLink to={"/dashboard/admin"}>Vendor Requests</NavLink>
+            <div className="logo">
+                <h1>FOOD<span>EX</span></h1>
             </div>
+
+            <div className="dashLinks">
+                <NavLink to={"/dashboard/admin"}><GoGitPullRequest />Vendor Requests</NavLink>
+                <NavLink to={"/dashboard/myshop"}><FaShopLock />My shop</NavLink>
+                <NavLink to={"/dashboard/users"}><FaRegUser />All User</NavLink>
+                <NavLink to={"/dashboard/members"}><FaUserTie />All Vendor</NavLink>
+            </div>
+
+
         </div>
     );
 };

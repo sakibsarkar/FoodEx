b841c78cf005e675879e0ad93227629332929882
sakibsarkar <sakibsarkar707@gmail.com>
2024-02-18 17:16:31 +0600

    after navigation scrolling bug fixed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,13 @@
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
+import ScrollToTopOnPageChange from "../Hooks & Functions/ScrollToTopOnPageChange";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
     return (
         <>
+            <ScrollToTopOnPageChange />
             <Navbar />
-           
             <Outlet />
             <Footer />
         </>
